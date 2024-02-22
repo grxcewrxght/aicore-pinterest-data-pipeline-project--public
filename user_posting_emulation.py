412d8f6381a45c540ff5d47b0e392db90689bb3e
Grace Wright <148906878+grxcewrxght@users.noreply.github.com>
2024-02-22 17:32:20 +0000

    Update user_posting_emulation.py
    Creates functions to simplify the code overall

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -54,7 +54,64 @@ class AWSDBConnector:
         return engine
 
 
-new_connector = AWSDBConnector()
+def fetch_random_row(connection, table_name, random_row):
+    """
+    Fetches a random row from the specified table in the database.
+    
+    Args:
+        connection (Connection): SQLAlchemy database connection object.
+        table_name (str): Name of the table to fetch data from.
+        random_row (int): Index of the random row to fetch.
+        
+    Returns:
+        dict: Dictionary containing the data from the fetched row.
+    """
+    query = text(f"SELECT * FROM {table_name} LIMIT {random_row}, 1")
+    selected_row = connection.execute(query)
+    for row in selected_row:
+        return dict(row._mapping)
+
+
+def create_payload(data):
+    """
+    Creates a JSON payload from the provided data.
+    
+    Args:
+        data (dict): Dictionary containing the data to be included in the payload.
+        
+    Returns:
+        str: JSON representation of the payload.
+    """
+    payload = {
+        "records": [
+            {
+                "value": data
+            }
+        ]
+    }
+    return json.dumps(payload)
+
+
+def post_data_to_topic(topic, base_url, headers, data):
+    """
+    Posts data to a specific topic using the provided URL, headers, and payload.
+    
+    Args:
+        topic (str): The topic to post data to.
+        base_url (str): The base URL of the API endpoint.
+        headers (dict): Headers for the HTTP request.
+        data (str): JSON payload containing the data to be posted.
+        
+    Returns:
+        None
+    """
+    invoke_url = f"{base_url}/topics/12a3da8f7ced.{topic}"
+    response = requests.post(invoke_url, headers=headers, data=data)
+    
+    if response.status_code == 200:
+        print(f"{topic.capitalize()} data posted successfully")
+    else:
+        print(f"Error posting {topic} data. Status Code: {response.status_code}, Content: {response.text}")
 
 
 def run_infinite_post_data_loop():
@@ -62,79 +119,41 @@ def run_infinite_post_data_loop():
     A function to continuously post data to an API endpoint.
     """
     headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+    base_url = "https://amdpzoul4j.execute-api.us-east-1.amazonaws.com/test"
+    topics = ["pin", "geo", "user"]
 
-    # Create an infinite loop for continuous data posting
     while True:
         sleep(random.randrange(0, 2))
         random_row = random.randint(0, 11000)
-        engine = new_connector.create_db_connector()
-        base_url = "https://amdpzoul4j.execute-api.us-east-1.amazonaws.com/test"
-        topics = ["pin", "geo", "user"]
+        engine = AWSDBConnector().create_db_connector()
 
-        # Connect to the database 
         with engine.connect() as connection:
+            pin_result = fetch_random_row(connection, "pinterest_data", random_row)
+            geo_result = fetch_random_row(connection, "geolocation_data", random_row)
+            user_result = fetch_random_row(connection, "user_data", random_row)
 
-            # Fetch random rows from the database
-            
-            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
-            pin_selected_row = connection.execute(pin_string)
-            
-            for row in pin_selected_row:
-                pin_result = dict(row._mapping)
-
-            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
-            geo_selected_row = connection.execute(geo_string)
-            
-            for row in geo_selected_row:
-                geo_result = dict(row._mapping)
-
-            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
-            user_selected_row = connection.execute(user_string)
-
-            for row in user_selected_row:
-                user_result = dict(row._mapping)
-
-            
-            # Prepare JSON payloads for posting data to the API
-            payload_pin = json.dumps({
-                "records": [
-                    {
-                        "value": {"index": pin_result['index'], "unique_id": pin_result["unique_id"],
-                                  "title": pin_result["title"], "description": pin_result["description"],
-                                  "poster_name": pin_result["poster_name"], "follower_count": pin_result["follower_count"],
-                                  "tag_list": pin_result["tag_list"], "is_image_or_video": pin_result["is_image_or_video"],
-                                  "image_src": pin_result["image_src"], "downloaded": pin_result["downloaded"],
-                                  "save_location": pin_result["save_location"], "category": pin_result["category"]}
-                    }
-                ]
+            payload_pin = create_payload({
+                "index": pin_result['index'], "unique_id": pin_result["unique_id"],
+                "title": pin_result["title"], "description": pin_result["description"],
+                "poster_name": pin_result["poster_name"], "follower_count": pin_result["follower_count"],
+                "tag_list": pin_result["tag_list"], "is_image_or_video": pin_result["is_image_or_video"],
+                "image_src": pin_result["image_src"], "downloaded": pin_result["downloaded"],
+                "save_location": pin_result["save_location"], "category": pin_result["category"]
             })
 
-            payload_geo = json.dumps({
-                "records": [
-                    {
-                        "value": {"index": geo_result["ind"], "country": geo_result["country"],
-                                  "timestamp": geo_result["timestamp"].isoformat(),
-                                  'latitude': geo_result["latitude"], "longitude": geo_result["longitude"]}
-                    }
-                ]
+            payload_geo = create_payload({
+                "index": geo_result["ind"], "country": geo_result["country"],
+                "timestamp": geo_result["timestamp"].isoformat(),
+                'latitude': geo_result["latitude"], "longitude": geo_result["longitude"]
             })
 
-            payload_user = json.dumps({
-                "records": [
-                    {
-                        "value": {"index": user_result["ind"], "first_name": user_result["first_name"],
-                                  "last_name": user_result["last_name"], "age": user_result["age"],
-                                  "date_joined": user_result["date_joined"].isoformat()}
-                    }
-                ]
+            payload_user = create_payload({
+                "index": user_result["ind"], "first_name": user_result["first_name"],
+                "last_name": user_result["last_name"], "age": user_result["age"],
+                "date_joined": user_result["date_joined"].isoformat()
             })
 
-
-            # Iterate over topics and post data to the API
             for topic in topics:
-                invoke_url = f"{base_url}/topics/12a3da8f7ced.{topic}"
-
-                # Select payload based on the topic 
                 if topic == "pin":
                     topic_data = payload_pin
                 elif topic == "geo":
@@ -142,11 +161,8 @@ def run_infinite_post_data_loop():
                 elif topic == "user":
                     topic_data = payload_user
 
-                # Make a HTTP POST request to the API
-                response = requests.request("POST", invoke_url, headers=headers, data=topic_data)
+                post_data_to_topic(topic, base_url, headers, topic_data)
 
-                # Print status message based on the response 
-                if response.status_code == 200:
-                    print(f"{topic.capitalize()} data posted successfully")
-                else:
-                    print(f"Error posting {topic} data. Status Code: {response.status_code}, Content: {response.text}")
+
+if __name__ == "__main__":
+    run_infinite_post_data_loop()
